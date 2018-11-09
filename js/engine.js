dde858ff6b22ea4aa8ea0d1c189f7282bc3ed3bc
josaphine <35378135+josaphinejoe@users.noreply.github.com>
2018-11-09 20:45:04 +0530

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,18 +1,3 @@
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.
- */
 
 var Engine = (function(global) {
   /* Predefine the variables we'll be using within this scope,
